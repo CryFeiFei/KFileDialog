a4b9eabdb3de389275175db31f5044155614535a
CryFeiFei <809151882@qq.com>
2019-07-14 18:46:25 +0800

    [添加解析当前目录的代码]

--- KFileDialog/kfileitemmodel.cpp
+++ KFileDialog/kfileitemmodel.cpp
@@ -1,5 +1,9 @@
 #include "kfileitemmodel.h"
 
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+
 KFileItemNode::KFileItemNode(FileType fileType, KFileItemNode *parent, const QString &fileName)
 	: m_fileType(fileType)
 	, m_parent(parent)
@@ -13,7 +17,92 @@ KFileItemNode::~KFileItemNode()
 	qDeleteAll(m_children);
 }
 
-KFileItemModel::KFileItemModel()
+KFileItemModel::KFileItemModel(QObject* parent/* = nullptr*/, const QString& rootPath)
+	: QAbstractItemModel(parent)
+	, m_rootNode(nullptr)
+{
+	m_strRootPath = QDir::toNativeSeparators(rootPath);
+}
+
+KFileItemModel::~KFileItemModel()
 {
 
+}
+
+void KFileItemModel::_init()
+{
+
+}
+
+void KFileItemModel::_createTree()
+{
+	QFileInfo rootInfo(m_strRootPath);
+	if (!rootInfo.exists())
+	{
+		Q_ASSERT("not exists");
+		return;
+	}
+
+	if (rootInfo.isFile())
+	{
+		Q_ASSERT("is File");
+		return;
+	}
+
+	_destroyTree();
+
+	if (rootInfo.isDir())
+	{
+		m_rootNode = new KFileItemNode(KFileItemNode::Folder, nullptr, m_strRootPath);
+		if (m_rootNode)
+		{
+			_createChildren();
+		}
+	}
+}
+
+void KFileItemModel::_destroyTree()
+{
+	if (m_rootNode)
+	{
+		m_rootNode->m_children.clear();
+		delete m_rootNode;
+		m_rootNode = nullptr;
+	}
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+	resetInternalData();
+#endif
+}
+
+void KFileItemModel::_createChildren()
+{
+	//todo 这里要写过滤条件
+	QStringList m_listType;
+	m_listType<<"*.*";
+	QDir dir(m_strRootPath);
+	QFileInfoList infoList = dir.entryInfoList(m_listType, QDir::AllDirs | QDir::NoDotAndDotDot | QDir::Files);
+	for (int i = 0; i < infoList.size(); i++)
+	{
+		QFileInfo fileInfo = infoList.at(i);
+		QString childFileName = fileInfo.absoluteFilePath();
+
+		KFileItemNode::FileType fileType;
+		if (fileInfo.isFile())
+		{
+			fileType = KFileItemNode::File;
+		}
+		else if (fileInfo.isDir())
+		{
+			fileType = KFileItemNode::Folder;
+		}
+		else
+		{
+			continue ;
+		}
+
+		KFileItemNode* childNode = new KFileItemNode(fileType, m_rootNode, childFileName);
+		if (childNode)
+			m_rootNode->m_children.append(childNode);
+
+	}
 }
--- KFileDialog/kfileitemmodel.h
+++ KFileDialog/kfileitemmodel.h
@@ -2,6 +2,7 @@
 #define KFILEITEMMODEL_H
 
 #include <QAbstractItemModel>
+#include <QFileInfo>
 
 class KFileItemNode
 {
@@ -21,10 +22,27 @@ public:
 	QList<KFileItemNode*> m_children;
 };
 
-class KFileItemModel
+//只加载当前目录的所有的文件以及文件夹
+//由于性能问题，暂时还要把加载过程放到新的线程里。
+class KFileItemModel : public QAbstractItemModel
 {
 public:
-	KFileItemModel();
+	explicit KFileItemModel(QObject* parent = nullptr, const QString& rootPath = QString());
+	~KFileItemModel();
+
+public:
+
+private:
+	void _init();
+	void _createTree();
+	void _destroyTree();
+	void _createChildren();
+
+private:
+	QString m_strRootPath;
+	QFileInfo m_rootFileInfo;
+	KFileItemNode* m_rootNode;
+
 };
 
 #endif // KFILEITEMMODEL_H
